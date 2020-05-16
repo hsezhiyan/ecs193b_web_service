7d930792d8fcb2d40b6f67e8daf91b05943cc37f
Hari <hsezhiyan@ucdavis.edu>
2020-05-15 21:31:50 -0700

    Adding mostly bug free svm model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect
 from sklearn.svm import SVC # for the SVM cath prediction model
 
+from processing_code import process_svm_dict
+
 import joblib 
 
 app = Flask(__name__)
@@ -18,25 +20,36 @@ def svm_cath_calc():
 	loaded_model = joblib.load("svm_cath.sav")
 	cath_data = request.args.to_dict()
 
-	print(cath_data)
+	try:
+		cath_data_list = process_svm_dict(cath_data)
+	except Exception as error:
+		return str(error)
 
-	cath_data_list = []
-	for key in cath_data:
-		try:
-			float(cath_data[key])
-			float_bool = True
-		except:
-			float_bool = False
+	# print(cath_data_list)
 
-		if float_bool:
-			cath_data_list.append(float(cath_data[key]))
-		elif cath_data[key] == "":
-			cath_data_list.append(-1)
+	# cath_data_list = []
+	# for key in cath_data:
+	# 	try:
+	# 		float(cath_data[key])
+	# 		float_bool = True
+	# 	except:
+	# 		float_bool = False
 
-	print((cath_data_list))
+	# 	if float_bool:
+	# 		cath_data_list.append(float(cath_data[key]))
+	# 	elif cath_data[key] == "":
+	# 		cath_data_list.append(-1)
+
+	# should be 1
+	# cath_data_list = [66, 1, 1, 0, 0,  0, 0,	0,	0,	175.0,	87.6,	0,	1,	0,	0,	0,	-1,	2,	2,	1,	1,	2,	4,	1,	0,	0.0,	0,	0.0,	0,	0.5,	0,	0.9,	0,	0.0,	1,	1,	1,	1,	1,	1]
+	
+	# should be 0
+	# cath_data_list = [63,	0,	1,	1,	0,	0,	1,	0,	0,	188.0,	71.0,	0,	0,	0,	0,	0,	-1,	1,	-1,	1,	0,	2,	1,	2,	0,	0.0,	0,	0.0,	0,	0.0,	0,	0.0,	0,	0.0,	1,	1,	1,	1,	1,	1]
+
 	cath_prediction = loaded_model.predict([cath_data_list])
 	return_string = f"Cath result is {cath_prediction}"
 	return return_string
 
 if __name__ == "__main__":
 	app.run()
+
---
+++ processing_code.py
@@ -0,0 +1,127 @@
+
+
+def process_svm_dict(svm_dict):
+	cath_data_list = []
+
+	# These are fields in which the user needs to input a float value (or leave blank)
+	float_value_keys = ["Age", "HeightCM", "WeightKG", "ProcOutcomesLMStenosis", 
+						"ProcOutcomesLADStenosis", "ProcOutcomesLADDistStenosis", 
+						"ProcOutcomesCIRCStenosis", "ProcOutcomesRCAStenosis"]
+
+	count1 = 0
+	count2 = 0
+	count3 = 0
+
+	print(svm_dict)
+
+	for i, key in enumerate(svm_dict):
+
+		print(i)
+
+		# Column S in CleanData.csv
+		if key == "DiabetesTherapy":
+			if svm_dict[key] == "":
+				cath_data_list.append(-1)
+			elif svm_dict[key] == "Diet":
+				cath_data_list.append(0)
+			elif svm_dict[key] == "Insulin":
+				cath_data_list.append(1)
+			elif svm_dict[key] == "None":
+				cath_data_list.append(2)
+			else:
+				cath_data_list.append(3)
+
+		# Column T in CleanData.csv
+		elif key == "StudyStressSPECTMRIResult":
+			if svm_dict[key] == "Indeterminant":
+				cath_data_list.append(0)
+			elif svm_dict[key] == "Negative":
+				cath_data_list.append(1)
+			elif svm_dict[key] == "Positive":
+				cath_data_list.append(2)
+			else:
+				cath_data_list.append(3)
+
+		# Column U in CleanData.csv
+		elif key == "StudyStressSPECTMRIIschemia":
+			if svm_dict[key] == "":
+				cath_data_list.append(-1)
+			elif svm_dict[key] == "High":
+				cath_data_list.append(1)
+			elif svm_dict[key] == "Intermediate":
+				cath_data_list.append(2)
+			elif svm_dict[key] == "Low":
+				cath_data_list.append(3)
+			else:
+				cath_data_list.append(4)
+
+		# Column X in CleanData.csv
+		elif key == "Scheduling_Type":
+			if svm_dict[key] == "":
+				cath_data_list.append(-1)
+			elif svm_dict[key] == "Elective":
+				cath_data_list.append(0)
+			elif svm_dict[key] == "Emergency":
+				cath_data_list.append(1)
+			else:
+				cath_data_list.append(2)
+
+		# Column Y in CleanData.csv
+		elif key == "PostDiagRxRecommendation":
+			if svm_dict[key] == "":
+				cath_data_list.append(-1)
+			elif svm_dict[key] == "CABG":
+				cath_data_list.append(0)
+			elif svm_dict[key] == "Medical Therapy/counseling":
+				cath_data_list.append(1)
+			elif svm_dict[key] == "None":
+				cath_data_list.append(2)
+			elif svm_dict[key] == "Other Therapy with CABG or PCI":
+				cath_data_list.append(3)
+			else:
+				cath_data_list.append(4)
+
+		# Column Z in CleanData.csv
+		elif key == "CoronaryDominance":
+			if svm_dict[key] == "":
+				cath_data_list.append(-1)
+			elif svm_dict[key] == "Codominant":
+				cath_data_list.append(0)
+			elif svm_dict[key] == "Left":
+				cath_data_list.append(1)
+			else:
+				cath_data_list.append(2)
+
+		elif key in float_value_keys:
+			if svm_dict[key] == "": # left empty
+				cath_data_list.append(-1)
+				continue
+
+			try:
+				float_val = float(svm_dict[key])
+			except:
+				print("Raising float value exception.")
+				raise Exception("Form element {} should be a float.".format(key))
+
+			cath_data_list.append(float_val)
+
+		else:
+			if svm_dict[key] == "": # left empty
+				cath_data_list.append(0)
+			elif svm_dict[key] == "No":
+				cath_data_list.append(0)
+			else:
+				cath_data_list.append(1)
+
+
+	return cath_data_list
+
+
+
+
+
+
+
+
+
+		
